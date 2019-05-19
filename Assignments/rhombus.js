77a44341b5a65ccabc7810d3d7a2efe91105af96
jazzyjaz <49316548+jazzyjaz@users.noreply.github.com>
2019-05-18 21:13:49 -0700

    Update rhombus.js

--- Assignments/rhombus.js
+++ Assignments/rhombus.js
@@ -8,6 +8,63 @@ upRight(pHeight, pColorEven, pColorOdd, pSymbol);
 downRight(pHeight, pColorEven, pColorOdd, pSymbol);
 
 }
+function downLeft(pHeight, pColorEven, pColorOdd, pSymbol){
+  var rLine ="";
+  for (i=pHeight;i>0;i--){
+  rLine +="<p>";
+  //rLine +="<span class='space'>" + pSymbol +"</span>";
+  // Fill in the left side of every row with space or blanks
+  for(x=1; x<=pHeight-i;x++){
+    //rLine +="-";
+    rLine +="<span class='space'>" +pSymbol + "</span>";
+  }
+  //Create each line on the Rhombus
+  for(j=x;j<=pHeight;j++){
+
+    //Is the position even or odd so we change the color
+    if (j%2)
+    //even
+    rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+    else
+    //odd
+    rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+  }
+  rLine +="</p>";
+  // console.log(rLine);
+  }
+  document.getElementById("downLeft").innerHTML = rLine;
+}
+
+
+function upLeft(pHeight, pColorEven, pColorOdd, pSymbol){
+var rLine ="";
+for (i=1;i<=pHeight;i++){
+rLine +="<p>";
+//Fill in the left side of every row with spaces or blanks 
+for (x=1; x<=pHeight-i;x++{
+rLine +="-";
+}
+
+//Create each line on the Rhombus
+for(j=x;j<=pHeight;j++){
+
+//Is the position even or odd so we change the color
+if (j%2) 
+//even
+rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+else
+//odd
+rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+
+}
+rLine +="</p>";
+// console.log(rLine);
+
+}
+
+document.getElementById("upLeft").innerHTML = rLine;
+}
+
 
 function upRight(pHeight, pColorEven, pColorOdd, pSymbol){
 var rLine ="";
